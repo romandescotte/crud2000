c071799111c19db2067b205b11544d9113106248
Roman Descotte <55287661+sk8mumy@users.noreply.github.com>
2021-12-26 14:05:12 -0300

    elimina URI de __init__ y lo agrega como variable de entorno. Agrega config.py

--- project/__init__.py
+++ project/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from project.config import Config
 
 app=Flask(__name__)
+app.config.from_object(Config)
 
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://bdac5df4f61814:6aa32f11@us-cdbr-east-05.cleardb.net/heroku_abb3b37cb667098'
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+#aca iba el app.config que borre para que vaya por variables de entorno
 
 db= SQLAlchemy(app)
 ma=Marshmallow(app)
